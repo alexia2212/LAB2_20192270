4e9271b46f6092d921ae24fdc674a14d5217657b
alexia2212 <a20192270@pucp.edu.pe>
2023-09-17 12:31:58 -0500

    vistas cronometro y contador

--- app/src/main/java/com/example/lab2_20192270/Cronometro.java
+++ app/src/main/java/com/example/lab2_20192270/Cronometro.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Login extends AppCompatActivity {
+public class Cronometro extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_cronometro);
     }
 }
